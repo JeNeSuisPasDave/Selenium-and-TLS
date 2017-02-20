4f1b88968967f3fbae52d499213031d56eb53337
Dave Hein <jenesuispasdave@gmail.com>
2017-02-20 13:18:05 -0600

    Update license boilerplate

--- helloworld-tls/src/tests/test_selenium_ff.py
+++ helloworld-tls/src/tests/test_selenium_ff.py
@@ -1,3 +1,8 @@
+# Copyright 2017 David Hein
+#
+# Licensed under the MIT License. If the LICENSE file is missing, you
+# can find the MIT license terms here: https://opensource.org/licenses/MIT
+
 import re
 import threading
 import time
